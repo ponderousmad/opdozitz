b35f1428dba9f853eb7e09fd16a256dfdd69540b
Adrian Smith <agnomen@gmail.com>
2016-01-04 21:39:53 -0500

    Modularize imageBatch

--- src/imageBatch.js
+++ src/imageBatch.js
@@ -1,49 +1,53 @@
-"use strict";
-
 var rootURL = "/scrace/";
 
-function ImageBatch(basePath, onComplete) {
-    this._toLoad = 0;
-    this._commited = false;
-    this._basePath = basePath;
-    this._onComplete = onComplete;
-    this.loaded = false;
-}
+var ImageBatch = (function (baseURL) {
+    "use strict";
 
-ImageBatch.prototype.setPath = function (path) {
-    this._basePath = path;
-};
-
-ImageBatch.prototype._checkComplete = function () {
-    if (this._commited) {
-        if (this._toLoad === 0) {
-            this.loaded = true;
-            if (this._onComplete) {
-                this._onComplete();
-            }
-        }
+    function ImageBatch(basePath, onComplete) {
+        this._toLoad = 0;
+        this._commited = false;
+        this._basePath = basePath;
+        this._onComplete = onComplete;
+        this.loaded = false;
     }
-};
 
-ImageBatch.prototype.load = function (resource, onLoad) {
-    this._toLoad += 1;
-    var image = new Image();
-    var self =  this;
-    image.onload = function () {
-        if (onLoad) {
-            onLoad(image);
-        }
-        self._toLoad -= 1;
-        self._checkComplete();
+    ImageBatch.prototype.setPath = function (path) {
+        this._basePath = path;
     };
 
-    var path = rootURL + (this._basePath || "") + resource;
+    ImageBatch.prototype._checkComplete = function () {
+        if (this._commited) {
+            if (this._toLoad === 0) {
+                this.loaded = true;
+                if (this._onComplete) {
+                    this._onComplete();
+                }
+            }
+        }
+    };
 
-    image.src = path;
-    return image;
-};
+    ImageBatch.prototype.load = function (resource, onLoad) {
+        this._toLoad += 1;
+        var image = new Image();
+        var self =  this;
+        image.onload = function () {
+            if (onLoad) {
+                onLoad(image);
+            }
+            self._toLoad -= 1;
+            self._checkComplete();
+        };
 
-ImageBatch.prototype.commit = function () {
-    this._commited = true;
-    this._checkComplete();
-};
+        var path = baseURL + (this._basePath || "") + resource;
+
+        image.src = path;
+        return image;
+    };
+
+    ImageBatch.prototype.commit = function () {
+        this._commited = true;
+        this._checkComplete();
+    };
+    
+    return ImageBatch;
+}(rootURL));
