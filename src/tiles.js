0e3e54f21580e938245bc4c22eba4a5240bb7ac1
Adrian Smith <agnomen@gmail.com>
2016-01-20 21:09:32 -0500

    Shim for isInteger (ie workaround)

--- src/tiles.js
+++ src/tiles.js
@@ -37,6 +37,13 @@
         tileBatch.commit();
     }());
     
+    // Shim from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+    Number.isInteger = Number.isInteger || function(value) {
+        return typeof value === "number" && 
+            isFinite(value) && 
+            Math.floor(value) === value;
+    };
+    
     function Tile(parts, left, top) {
         if (Number.isInteger(parts)) {
             this.parts = parts;
